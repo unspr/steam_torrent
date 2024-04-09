fcee2f5b4edb6ec9886ae98ec02ee6d2fff617fc
UnSpr <helloworldspring@gmail.com>
2024-04-09 12:39:12 +0000

    update output if clicking selector

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const textarea = document.getElementById('inputText');
 const outPutText = document.getElementById('outputText');
 const mode = document.getElementById('mode');
 
+function updateOutPut() {
+  outPutText.value = setup(breakdown(textarea.value), mode.value);
+}
 
-textarea.addEventListener('input', function(event) {
-  outPutText.value = setup(breakdown(textarea.value), mode.value);;
-});
+textarea.addEventListener('input', updateOutPut);
+mode.addEventListener('change', updateOutPut);
